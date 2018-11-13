44a9d02e3157c5ae258c4c64aefc1aa33eb21591
Vitaly Li <>
2018-11-13 18:47:28 -0500

    added solution to "Find All Numbers Disappeared in an Array" problem

---
+++ JS/findDisappearedNumbers.js
@@ -0,0 +1,36 @@
+/**
+ * 
+ * Given an array of integers where 1 ≤ a[i] ≤ n (n = size of array),
+ * some elements appear twice and others appear once.
+ * Find all the eleme
+ * 
+ * Example
+ * Input:
+ * [4,3,2,7,8,2,3,1]
+ * Output:
+ * [5,6]
+ * 
+ */
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var findDisappearedNumbers = function(nums) {
+    var resultArray = [];
+    var tempArray = new Array(nums.length + 1);
+
+    for (let i = 0; i < nums.length; i++) {
+      let el = nums[i];
+      
+      tempArray[el] = el;
+    }
+
+    for (let i = 1; i < tempArray.length; i++) {
+      if (tempArray[i] === undefined) {
+        resultArray.push(i);
+      }
+    }
+
+    return resultArray;
+};
