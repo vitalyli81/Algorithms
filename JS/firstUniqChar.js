5782fb20af6764456db3ea3dd0a33815f0c58fdf
Vitaly Li <>
2018-11-13 17:59:47 -0500

    added solution to 'firstUniqChar' problem

--- JS/firstUniqChar.js
+++ JS/firstUniqChar.js
@@ -3,7 +3,7 @@
  * Given a string, find the first non-repeating character in it and return it's index. 
  * If it doesn't exist, return -1.
  * Example :
- * 'abbcc' returns 0
+ * 'abbccd' returns 0
  * 'ababc' returns 4
  * 'aabbcc' returns -1
  */
@@ -13,5 +13,20 @@
  * @return {number}
  */
 var firstUniqChar = function(s) {
-    
+    var array = s.split('');
+    var unique = new Set();
+    var symHash = {};
+
+    for (let i = 0; i < array.length; i++ ) {
+      var el = array[i];
+
+      if (symHash[el] === undefined) {
+        unique.add(el);
+        symHash[el] = i;
+      } else {
+        unique.delete(el);
+      }
+    }
+
+    return !unique.size && -1 || symHash[unique.values().next().value];
 };
