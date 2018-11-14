e4ebd8f661083c5c19ca8b1de935ff994c765449
Vitaly Li <>
2018-11-13 21:30:39 -0500

    two sum problem

---
+++ randomProblems/JS/twoSum.js
@@ -0,0 +1,38 @@
+/**
+ * 
+ * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ * 
+ * Example
+ * 
+ * Given nums = [2, 7, 11, 15], target = 9,
+ * Because nums[0] + nums[1] = 2 + 7 = 9,
+ * return [0, 1].
+ * 
+ */
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+  var complementObj = {},
+      result = [],
+      length = nums.length;
+  
+  for (var i=0; i < length; i++) {
+      var el = nums[i];
+      var complement = target - el;
+      
+      if ( complementObj[el] !== undefined) {
+          result.push(complementObj[el]);
+          result.push(i);
+          break
+      }
+      
+      complementObj[complement] = i;
+  }
+  
+  return result;
+};
