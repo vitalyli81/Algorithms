1f666c714fea8dd0fec7565a405023d02ba1f9a8
Vitaly Li <>
2018-11-15 20:38:29 -0500

    encode decode url problem

---
+++ randomProblems/JS/encodeDecodeTinyURL.js
@@ -0,0 +1,36 @@
+/**
+ * 
+ * Design the encode and decode methods for the TinyURL service.
+ * There is no restriction on how your encode/decode algorithm should work.
+ * You just need to ensure that a URL can be encoded to a tiny URL and the tiny URL can be decoded to the original URL.
+ */
+
+/**
+ * Encodes a URL to a shortened URL.
+ *
+ * @param {string} longUrl
+ * @return {string}
+ */
+var encode = function(longUrl) {
+    var url = `http://tinyurl.com/${Math.random(100000000000000)}`;
+    hash[url] =  `${longUrl}`;
+
+    return url;
+};
+
+var hash = {};
+
+/**
+ * Decodes a shortened URL to its original URL.
+ *
+ * @param {string} shortUrl
+ * @return {string}
+ */
+var decode = function(shortUrl) {
+    return hash[shortUrl];
+};
+
+/**
+ * Your functions will be called as such:
+ * decode(encode(url));
+ */
