8fb891516844de7c58dc0d12b91ac41574e2358e
Vitaly Li <>
2018-11-18 14:38:08 -0500

    solved problem "meetings room"

--- randomProblems/JS/meetingRooms2.js
+++ randomProblems/JS/meetingRooms2.js
@@ -1,3 +1,52 @@
+/**
+ * 
+ * Given an array of meeting time intervals consisting of start and end times [[s1,e1],[s2,e2],...] (si < ei),
+ * find the minimum number of conference rooms required.
+ * 
+ * Example 1:
+ * Input: [[0, 30],[5, 10],[15, 20]]
+ * Output: 2
+ * 
+ * Example 2:
+ * 
+ * Input: [[7,10],[2,4]]
+ * Output: 1
+ * 
+ */
+
+/**
+ * Definition for an interval.
+ * function Interval(start, end) {
+ *     this.start = start;
+ *     this.end = end;
+ * }
+ */
+/**
+ * @param {Interval[]} intervals
+ * @return {number}
+ */
+var minMeetingRooms = function (intervals) {
+  if (intervals.length === 0) return 0;
+
+  var priorityQueue = new MinPriorityQueue([]);
+  var result = 1;
+
+  intervals.sort((a, b) => a.start - b.start);
+  priorityQueue.heapInsert(intervals[0].end);
+
+  for (let i = 1; i < intervals.length; i++) {
+    if (priorityQueue.getHeapMin() > intervals[i].start) {
+      priorityQueue.heapInsert(intervals[i].end);
+      result += 1;
+    } else {
+      priorityQueue.heapExtractMin();
+      priorityQueue.heapInsert(intervals[i].end);
+    }
+  }
+
+  return result;
+};
+
 function MinPriorityQueue(A) {
   this.A = A;
   this.heapSize = A.length;
@@ -7,7 +56,7 @@ function MinPriorityQueue(A) {
 
 MinPriorityQueue.prototype.buildMinHeap = function () {
   if (this.heapSize < 1) return;
-  
+
   var len = this.A.length;
   var mid = len % 2 ? (len - 1) / 2 : len / 2
 
