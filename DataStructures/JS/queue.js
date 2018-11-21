24f85fd26742732fa6e8c3d5965703214befc224
Vitaly Li <>
2018-11-20 19:27:46 -0500

    queue data structure

---
+++ DataStructures/JS/queue.js
@@ -0,0 +1,31 @@
+/**
+ * Queue data structure 
+ * First in First out
+ */
+
+function Queue() {
+  this.data = {};
+  this.size = 0;
+  this.tail = 0;
+  this.head = 0;
+}
+
+Queue.prototype.enqueue = function (val) {
+  this.data[++this.tail] = val;
+  this.size++;
+}
+
+Queue.prototype.dequeue = function () {
+ if (this.size < 1) return;
+
+ var elem = this.data[++this.head];
+
+ delete this.data[this.head];
+
+ this.size--;
+ 
+ if (this.size === 0) {
+   this.tail = 0;
+   this.head = 0;
+ }
+}
