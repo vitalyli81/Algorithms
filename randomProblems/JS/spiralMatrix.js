020b0416a0ac008dc13d5f42f2921516522a44d1
Vitaly Li <>
2018-11-30 18:19:24 -0500

    comments

--- randomProblems/JS/spiralMatrix.js
+++ randomProblems/JS/spiralMatrix.js
@@ -1,4 +1,22 @@
 /**
+ * 
+ * Given a matrix of m x n elements (m rows, n columns), return all elements of the matrix in spiral order.
+ * 
+ * 
+ * Example:
+ * 
+ * Input:
+ * [
+ *  [ 1, 2, 3 ],
+ *  [ 4, 5, 6 ],
+ *  [ 7, 8, 9 ]
+ * ]
+ * 
+ * Output: [1,2,3,6,9,8,7,4,5]
+ * 
+ */
+
+/**
  * @param {number[][]} matrix
  * @return {number[]}
  */
