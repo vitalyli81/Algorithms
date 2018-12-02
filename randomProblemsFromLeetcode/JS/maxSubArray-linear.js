436a6170ad9f3925192efa9af7004795fd634a53
Vitaly Li <>
2018-12-02 14:32:50 -0500

    refactoring

--- randomProblemsFromLeetcode/JS/maxSubArray-linear.js
+++ randomProblemsFromLeetcode/JS/maxSubArray-linear.js
@@ -4,9 +4,9 @@
  */
 
 var maxSubArray = function (nums) {
-  var sum = maxSum =  Number.NEGATIVE_INFINITY;
+  var sum = maxSum =  nums[0];
 
-  for (let i = 0; i < nums.length; i++) {
+  for (let i = 1; i < nums.length; i++) {
     sum = Math.max(sum + nums[i], nums[i]);
     maxSum = Math.max(maxSum, sum);
   }
