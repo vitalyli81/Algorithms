b0ddea62af8e617ab54fc0cf738a1475e50e969b
Vitaly Li <>
2018-12-01 23:44:06 -0500

    added recursive solution for merge 2 lists

--- randomProblemsFromLeetcode/JS/mergeTwoLists.js
+++ randomProblemsFromLeetcode/JS/mergeTwoLists.js
@@ -22,6 +22,8 @@
  * @param {ListNode} l2
  * @return {ListNode}
  */
+
+ // iterative approach
 var mergeTwoLists = function (l1, l2) {
   if (!l1) return l2;
   if (!l2) return l1;
@@ -51,3 +53,18 @@ var mergeTwoLists = function (l1, l2) {
 
   return l3.next;
 };
+
+// recursive approach
+
+var mergeTwoLists = function (l1, l2) {
+  if (!l1) return l2;
+  if (!l2) return l1;
+
+  if (l1.val < l2.val) {
+    l1.next = mergeTwoLists(l1.next, l2);
+    return l1;
+  } else {
+    l2.next  = mergeTwoLists(l1, l2.next);
+    return l2;
+  }
+}
