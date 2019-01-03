e35dc91cf6e37066f6012bba3c492eab7224083e
Vitaly Li <>
2019-01-02 20:37:12 -0500

    countPrimes

---
+++ randomProblemsFromLeetcode/JS/countPrimes.js
@@ -0,0 +1,38 @@
+/**
+ * 
+ * Count the number of prime numbers less than a non-negative number, n.
+ * 
+ * Example:
+ * 
+ * Input: 10
+ * Output: 4
+ * Explanation: There are 4 prime numbers less than 10, they are 2, 3, 5, 7.
+ * 
+ */
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var countPrimes = function (n) {
+  var tempArray = Array(n);
+  var res = 0;
+
+  for (let i = 2; i <= Math.floor(Math.sqrt(n)); i++) {
+    var m = 0;
+
+    if (tempArray[i] === undefined) {
+      for (let j = i * i; j < n; j = Math.pow(i, 2) + (m++) * i) {
+        tempArray[j] = null;
+      }
+    }
+  }
+
+  for (let i = 2; i < n; i++) {
+    if (tempArray[i] === undefined) {
+      res++;
+    }
+  }
+
+  return res;
+};
