e5130527605a1322c8632e76aebdbcd03833c3ed
Vitaly Li <>
2019-01-02 23:27:57 -0500

    moveZeroes

---
+++ randomProblemsFromLeetcode/JS/moveZeroes.js
@@ -0,0 +1,33 @@
+/**
+ * 
+ * Given an array nums, write a function to move all 0's to the end of it
+ *  while maintaining the relative order of the non-zero elements.
+ * 
+Example:
+
+Input: [0,1,0,3,12]
+Output: [1,3,12,0,0]
+Note:
+
+You must do this in-place without making a copy of the array.
+Minimize the total number of operations.
+ * 
+ */
+
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function (nums) {
+  var nonZeroIndex = 0;
+  var temp;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== 0) {
+      temp = nums[nonZeroIndex];
+      nums[nonZeroIndex] = nums[i];
+      nums[i] = temp;
+      nonZeroIndex++;
+    }
+  }
+};
