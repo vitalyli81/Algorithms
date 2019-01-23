8df856a559f8acffd6eac14438144c99539e61ec
Vitaly Li <>
2019-01-22 19:27:46 -0500

    fixed bug

--- randomProblemsFromLeetcode/JS/powerOfTwo.js
+++ randomProblemsFromLeetcode/JS/powerOfTwo.js
@@ -24,9 +24,8 @@ var isPowerOfTwo = function (n) {
   if (n === 1 || n === 2) return true;
   if (n % 2 || n === 0) return false;
 
-  var m = n;
-  while (m % 2 === 0) {
-    m = m / 2;
+  while (n % 2 === 0) {
+    n = n / 2;
   }
-  return m === 1 || false;
+  return n === 1 || false;
 };
