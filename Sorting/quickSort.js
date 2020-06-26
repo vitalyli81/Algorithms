4a164ef88077144f4f2e8c91845611c095390731
Vitaly Li <>
2020-06-25 23:27:40 -0400

    sort refactoring

--- Sorting/quickSort.js
+++ Sorting/quickSort.js
@@ -1,37 +1,33 @@
-var sort = function (A) {
+const sort = (A) => {
   if (A.length < 2) return A;
   quickSort(A, 0, A.length - 1);
-}
+};
 
-var quickSort = function (A, startIndex, endIndex) {
+const quickSort = (A, startIndex, endIndex) => {
   if (startIndex < endIndex) {
-    var mid = partition(A, startIndex, endIndex);
+    const mid = partition(A, startIndex, endIndex);
     quickSort(A, startIndex, mid - 1);
     quickSort(A, mid + 1, endIndex);
   }
-}
+};
 
-var partition = function (A, s, e) {
-  var pivot = A[e];
-  var y = s; // pointer to index where we will save pivot value
-  var temp;
+const partition = (A, s, e) => {
+  let pivot = s;
 
   for (let i = s; i < e; i++) {
-    if (A[i] <= pivot) {
-      //swap A[y] with A[i]
-      if (y !== i) {
-        temp = A[y];
-        A[y] = A[i];
-        A[i] = temp;
-      }
-      y++;
+    if (A[i] <= A[e]) {
+      swap(A, pivot, i);
+      pivot++;
     }
   }
-  // swap A[e] with A[y + 1]
-  if (e !== y) {
-    A[e] = A[y];
-    A[y] = pivot;
-  }
 
-  return y;
-}
+  swap(A, e, pivot);
+
+  return pivot;
+};
+
+const swap = (A, i, j) => {
+  const temp = A[i];
+  A[i] = A[j];
+  A[j] = temp;
+};
